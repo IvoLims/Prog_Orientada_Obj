c7c0622018a99e0b500e2fc3421f807d13913bc7
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-03-08 18:05:04 +0000

    Update TestI.java

--- Fichas/Guião2/TestI.java
+++ Fichas/Guião2/TestI.java
@@ -15,7 +15,44 @@ public class TestI{
     System.out.println("Introduza qual alinea quer: ");
     int alinea = ler.nextInt();
     switch(alinea) {
-                 case 1:
+                 case 1: int len1;
+                         do{ System.out.println("Diga quantos elementos tem o array : ");
+                             len1 = ler.nextInt();
+                           } while (len1 <= 0);
+                         int[] array1= new int[len1];
+                         System.out.println("Digite os elementos do array:");
+                         for(int i=0;i<len1;i++){
+                           array1[i] = ler.nextInt();
+                         }
+                         System.out.println("1: Valor mínimo desse array, ou 2: Array delimitado entre índices,ou 3: Array com os elementos comuns aos dois arrays:");
+                         int prob1 = ler.nextInt();
+                         switch(prob1){
+                                    case 1: ler.close();
+                                    int sol1 = exs.minInt(array1);
+                                    System.out.println("O menor valor do array é "+sol1);
+                                    break;
+                                    case 2: System.out.println("Digite o limite inferior e o superior: ");
+                                    int numsinf = ler.nextInt();
+                                    int numsup = ler.nextInt();
+                                    ler.close();
+                                    int[] sol2 = exs.limites(array1,numsinf,numsup);
+                                    System.out.println("O array delimitado fica assim: "+Arrays.toString(sol2));
+                                    break;
+                                    case 3: int len12;
+                                    do{ System.out.println("Diga quantos elementos tem o 2º array : ");
+                                    len12 = ler.nextInt();
+                                    } while (len12 <= 0);
+                                    int[] array12 = new int[len12];
+                                    System.out.println("Digite os elementos do array:");
+                                    for(int i=0;i<len12;i++){
+                                        array12[i] = ler.nextInt();
+                                    }
+                                    ler.close();
+                                    int[] sol3 = exs.compInt(array1,array12);
+                                    System.out.println("Os elementos comuns são "+Arrays.toString(sol3));
+                                    default:
+                                    break;
+                         }
                  break;
                  case 4: int len4;
                          do{ System.out.println("Diga quantos elementos tem o array que vai ordenar: ");
@@ -39,7 +76,7 @@ public class TestI{
                                     String res2 = exs.busca(array4,num);
                                     System.out.println("A busca teve resultado "+res2);
                                     break;
-                                    default: System.out.println("Essa alínea não existe, tente outra.");
+                                    default:
                                     break;
                          }
                  break;
@@ -77,14 +114,13 @@ public class TestI{
                                     int val4 = exs.vOcorre(array5,len5,comp);
                                     System.out.println("A String "+ comp +" ocorre "+ val4+ " vezes.");
                                     break;
-                                    default: ler.close();
-                                    System.out.println("Essa alínea não existe, tente outra.");
+                                    default: ler.close();System.out.println("Essa alínea não existe, tente outra.");
                                     break;
                          }
                  break;
-                 default: ler.close();
-                 System.out.println("Essa alínea não existe, tente outra.");
+                 default: System.out.println("Essa alínea não existe, tente outra.");
                  break;
                 }
   }
 }
+
