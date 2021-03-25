ae93395b3c1201f6b1e82976da82d1d65ec501b9
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-03-25 19:43:44 +0000

    Update Test42.java

--- Fichas/Guião4/Test42.java
+++ Fichas/Guião4/Test42.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class Test42 {
+public class Test {
        public static void main(String [] args){
               Scanner ler = new Scanner(System.in);
-              Ex42 exs = new Ex42("Ótima Átila Dantas", 12345678, "Rua Escadinhas da Porta do Carro", 665, LocalDate.now(), new ArrayList<LinhaEncomenda>());
+              Ex42 exs = new Ex42("Ótima Átila Dantas", 12345678, "Rua Escadinhas da Porta do Carro", 665, LocalDateTime.now(), new ArrayList<LinhaEncomenda>());
               System.out.println("\nA sua encomenda foi criada com os seguintes dados: " + exs.toString());
-              LinhaEncomenda linha1 = new LinhaEncomenda("11111", "Revista Maria", 5, 1, 6, 10);
-              exs.adicionaLinha(linha1);
-              System.out.println("Inserido na linha de encomenda: " + linha1.toString()+"\n");
-              LinhaEncomenda linha2 = new LinhaEncomenda("22222", "Perninhas de Morcego Recheadas", 20, 3, 15, 17.5);
-              exs.adicionaLinha(linha2);
-              System.out.println("" + linha2.toString()+"\n");
-              System.out.println("Introduza qual método quer ver: ");
+              LinhaEncomenda line1 = new LinhaEncomenda("11111", "Revista Maria", 5, 1, 6, 10);
+              exs.adicionaLinha(line1);
+              System.out.println("Inserido na linha de encomenda: " + line1.toString()+"\n");
+              LinhaEncomenda line2 = new LinhaEncomenda("22222", "Perninhas de Morcego Recheadas", 20, 3, 15, 17.5);
+              exs.adicionaLinha(line2);
+              System.out.println("" + line2.toString()+"\n");
+              System.out.println("Introduza qual método quer testar: ");
               int alinea = ler.nextInt();
               switch(alinea) {
                               case 2:{ 
@@ -29,11 +29,10 @@ public class Test42 {
                               case 4:{
                                       ler.close();
                                       System.out.println("\nVocê deverá receber "+exs.numeroTotalProdutos()+" produtos.\n");
-
                               break;
                               }
                               case 5:{
-                                      System.out.println("\nQual é a referência do produto procura?");
+                                      System.out.println("\nQual é a referência do produto que procura?");
                                       String refProduto = ler.next();
                                       ler.close();
                                       System.out.println("Podemos confirmar que o produto que procura, "+exs.existeProdutoEncomenda(refProduto)+" existe.\n");
@@ -41,11 +40,12 @@ public class Test42 {
                               break;
                               }
                               case 7:{
-                                      System.out.println("\nQual é o código do produto que quer remover?");
+                                      System.out.println("\nQual é o código do produto que deseja remover?");
                                       String codProd = ler.next();
+                                      System.out.println(codProd);
                                       ler.close();
                                       exs.removeProduto(codProd);
-                                      System.out.printf("O produto com o código "+codProd+", "+exs.existeProdutoEncomenda(codProd)+" foi removido.\n");
+                                      System.out.println("O produto com o código "+codProd+", "+exs.existeProdutoEncomenda(codProd)+" foi removido.\n");
                               break;
                               }
                               default:{
