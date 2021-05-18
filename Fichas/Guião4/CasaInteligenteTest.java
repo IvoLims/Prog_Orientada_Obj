3506f98a94f665862ce8818b1ab4e855a9aa8923
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-05-18 13:49:49 +0100

    Update CasaInteligenteTest.java

--- Fichas/Guião4/CasaInteligenteTest.java
+++ Fichas/Guião4/CasaInteligenteTest.java
@@ -1,5 +1,3 @@
-package casainteligente;
-
 /*********************************************************************************/
 /** DISCLAIMER: Este código foi criado e alterado durante as aulas práticas      */
 /** de POO. Representa uma solução em construção, com base na matéria leccionada */ 
