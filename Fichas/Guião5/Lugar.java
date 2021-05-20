458fcb1c8149ae800b15c0597715f0d6761b8944
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-05-20 20:35:03 +0100

    Create Lugar.java

---
+++ Fichas/Guião5/Lugar.java
@@ -0,0 +1,66 @@
+import java.util.Objects;
+
+public class Lugar implements Comparable<Lugar>{
+    private String matricula;
+    private String name;
+    private int min;
+    private boolean perm;
+
+    public Lugar(Lugar l) {
+        
+    }
+
+    public boolean equals(Object o) { 
+        if(this == o) return true;
+        if(o == null && this.getClass() != o.getClass()) return false;
+        Lugar l = (Lugar) o;
+        return this.matricula.equals(l.matricula) && this.name.equals(name) &&
+               this.min == l.min && this.perm == l.perm; 
+    }
+    /*or
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lugar)) return false;
+        Lugar lugar = (Lugar) o;
+        return min == lugar.min && perm == lugar.perm && Objects.equals(matricula, lugar.matricula) && Objects.equals(name, lugar.name);
+    }
+     */
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Matricula: ").append(this.matricula);
+        sb.append("\nName: ").append(this.name);
+        sb.append("\nTempo: ").append(this.min).append(" min\n");
+        sb.append("Lugar permanente: ").append(this.perm);
+        return sb.toString();        
+    }
+    public Lugar clone(){
+        return new Lugar(this);
+    }
+    public int compareTo(Lugar l){
+        return this.min - l.min;
+    }
+    public String getMatricula(){
+        return this.matricula;
+    }
+    public void setMatricula(String m){
+        this.matricula = m;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    public int getMin() {
+        return this.min;
+    }
+    public void setMin( int min){
+        this.min = min;
+    }
+    public boolean getPerm(){
+        return this.perm;
+    }
+    public void setPerm(boolean pe){
+        this.perm = pe;
+    }
+}
