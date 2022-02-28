5653e6fcb7cfbfdf9aa51a0287eec6d6fc03f5c1
IvoLims <75138193+IvoLims@users.noreply.github.com>
2022-02-28 17:30:19 +0000

    Create LinhaDeEncomenda.java

---
+++ Fichas/Guião3/LinhaDeEncomenda.java
@@ -0,0 +1,80 @@
+public class LinhaDeEncomenda {
+    private String codProd;
+    private String descricao;
+    private double preImposto;
+    private int quantidade;
+    private double imposto;
+    private double desComercial;
+    
+    public LinhaDeEncomenda(){
+        this.codProd = "";
+        this.descricao = "";
+        this.preImposto = 0;
+        this.quantidade = 0;
+        this.imposto = 0;
+        this.desComercial = 0;
+    }
+    public LinhaDeEncomenda(String ref, String desc, double pi, int q, double imp, double dCom){
+        this.codProd = ref;
+        this.descricao = desc;
+        this.preImposto = pi;
+        this.quantidade = q;
+        this.imposto = imp;
+        this.desComercial =  dCom;
+    }
+    public LinhaDeEncomenda(LinhaDeEncomenda lde){
+        this(lde.getCod(), lde.getDesc(), lde.getPreImp(), lde.getQ(), lde.getImp(), lde.getDComp());
+    }
+    public String getCod(){
+        return this.codProd;
+    }
+    public String getDesc(){
+        return this.descricao;
+    }
+    public double getPreImp(){
+        return this.preImposto;
+    }
+    public int getQ(){
+        return this.quantidade;
+    }
+    public double getImp(){
+        return this.imposto;
+    }
+    public double getDComp(){
+       return this.desComercial;
+    }
+    public void setCod(String newRef){
+        this.codProd = newRef;
+    }
+    public void setDesc(String newDesc){
+        this.descricao = newDesc;
+    }
+    public void setPreImp(double newPreImp){
+        this.preImposto = newPreImp;
+    }
+    public void setQ(int newQ){
+        this.quantidade = newQ;
+    }
+    public void setImp(double newImp){
+        this.imposto = newImp;
+    }
+    public void setDComp(double newdesComp){
+       this.desComercial = newdesComp;
+    }
+    public boolean equals(LinhaDeEncomenda lde){
+        if(this == lde) return true;
+        if(lde == null || this.getClass() != lde.getClass()) return false;
+        return this.getCod().equals(lde.getCod()) &&
+               this.getDesc().equals(lde.getDesc()) &&
+               this.getPreImp() == lde.getPreImp() &&
+               this.getQ() == lde.getQ() &&
+               this.getImp() == lde.getImp() &&
+               this.getDComp() == lde.getDComp();
+    }
+    public String toString(){
+        return "";
+    }
+    public LinhaDeEncomenda clone(){
+        return new LinhaDeEncomenda(this);
+    }
+}
