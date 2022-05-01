f514dc2f996f0a865e7f9adddbb076945287aae4
IvoLims <75138193+IvoLims@users.noreply.github.com>
2022-05-02 00:28:27 +0100

    Add files via upload

--- Fichas/Guião5/GestaoEncomendas.java
+++ Fichas/Guião5/GestaoEncomendas.java
@@ -1,4 +1,9 @@
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -9,7 +14,7 @@ public class GestaoEncomendas implements Comparable<GestaoEncomendas>{
         this.enc = Collections.emptyMap();
     }
     public GestaoEncomendas(Map<String,Encomenda> nEnc){
-        this.enc = nEnc.values().stream().collect(Collectors.toMap(Encomenda::getNumEnc, Encomenda::clone));
+        this.enc = nEnc.values().stream().collect(Collectors.toMap(Encomenda::getOrderN, Encomenda::clone));
     }
     public GestaoEncomendas(GestaoEncomendas gE){
         this.enc = gE.getEnc();
@@ -36,15 +41,59 @@ public class GestaoEncomendas implements Comparable<GestaoEncomendas>{
         return new GestaoEncomendas(this);
     }
     public Map<Integer,Encomenda> getEnc(){
-        return this.enc.values().stream().collect(Collectors.toMap(Encomenda::getNumEnc, Encomenda::clone));
+        return this.enc.values().stream().collect(Collectors.toMap(Encomenda::getOrderN, Encomenda::clone));
     }
     public void setEnc(Map<Integer,Encomenda> nEnc){
-        this.enc = nEnc.values().stream().collect(Collectors.toMap(Encomenda::getNumEnc, Encomenda::clone));
+        this.enc = nEnc.values().stream().collect(Collectors.toMap(Encomenda::getOrderN, Encomenda::clone));
     }
     public Set<Integer> todosOsCodigos(){
         return this.enc.keySet();
     }
     public void addEncomenda(Encomenda e){
-        this.enc.put(e.getNumEnc(), e.clone());
+        this.enc.put(e.getOrderN(), e.clone());
+    }
+    public void removeEncomenda(Integer codEnc){
+        this.enc.remove(codEnc);
+    }
+    public int encomendaComMaisProdutos(){
+        return this.enc.values().stream().min((a,b)->b.numProdutos() - a.numProdutos()).get().getOrderN();
+    }
+    public Set<Integer> encomendasComProduto(String codProd){
+        return this.enc.values().stream().filter(a -> a.existeNaEncomenda(codProd)).map(Encomenda::getOrderN).collect(Collectors.toSet());
+    }
+    public Set<Integer> encomendasAposData(LocalDate d){
+        return this.enc.values().stream().filter(a->a.getDate().isAfter(ChronoLocalDate.from(d))).map(Encomenda::getOrderN).collect(Collectors.toSet());
+    }
+    public Set<Encomenda> encomendasValorDecrescente(){
+        return this.enc.values().stream().map(Encomenda::clone).sorted((a,b) -> (int) (b.calculaValorTotal()-a.calculaValorTotal())).collect(Collectors.toSet());
+        /*
+        public Set<Encomenda> encomendasValorDecrescente(){
+            Comparator<Encomenda> comp=(e1,e2)->(int)(e2.calculaValorTotal()-e1.calculaValorTotal());
+            TreeSet <Encomenda> aux=new TreeSet<Encomenda>(comp);
+            for(Encomenda e:this.encomendas.values())
+                aux.add(e.clone());
+            return aux;
+        }
+       
+        public Collection<Encomenda> encomendasValorDecrescenteext(){ //Usa iteradores externos
+            List<Encomenda> aux=new ArrayList();
+            for(Encomenda e: this.encomendas.values())
+                aux.add(e.clone());
+            aux.sort((e1,e2)->(int)(e2.calculaValorTotal()-e1.calculaValorTotal()));
+            return aux;
+        }
+       */
+    }
+    public Map<String,List<Integer>> encomendasDeProduto(){
+        Map<String,List<Integer>> aux=new HashMap<>();
+        for(Encomenda e:this.enc.values()){
+            List<String> lprods=e.getListaProdutos();
+            for(String codProd: lprods){
+               if(!aux.containsKey(codProd))
+                    aux.put(codProd,new ArrayList<Integer>());
+               aux.get(codProd).add(e.getOrderN());
+            }
+         }
+        return aux;
     }
 }
