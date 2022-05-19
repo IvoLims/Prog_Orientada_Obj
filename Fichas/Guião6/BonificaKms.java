0aba4482c32cf0a5e73345503d1b94f921cfcb7e
IvoLims <75138193+IvoLims@users.noreply.github.com>
2022-05-19 16:14:36 +0100

    Update BonificaKms.java

--- Fichas/Guião6/BonificaKms.java
+++ Fichas/Guião6/BonificaKms.java
@@ -1,10 +1,3 @@
-/**
- * Interface de atribuição de pontos de fidelidade.
- * X pontos por Km.
- *
- * @author anr
- * @version 20210505
- */
 public interface BonificaKms {
     
     //Definir o valor de pontos a atribuir por cada km.
